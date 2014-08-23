f4407757d7257d7e4839bce1d1f3bbd6083c8b84
Jos Kuijpers <jos@kuijpersvof.nl>
2014-08-23 16:15:36 +0200

    Add post and call methods (latter not fully implemented).

--- test/easyideal.test.js
+++ test/easyideal.test.js
@@ -5,18 +5,24 @@ var EasyIdeal = require('../lib/easyideal'),
     assert = require('assert');
 
 describe('Easy Ideal', function () {
-    it('should do stuff', function () {
-        var easyideal = new EasyIdeal({
-            merchant: {
-                id: 'SomeId',
-                key: 'MyKey',
-                secret: 'SomeSecret'
-            }
+    describe('#banks()', function () {
+        it('should do stuff', function (done) {
+            var easyideal = new EasyIdeal({
+                merchant: {
+                    id: 'A',
+                    key: 'A',
+                    secret: 'A'
+                }
+            });
+
+            easyideal.banks(function (err, results) {
+                if (err)
+                    throw err;
+
+
+
+                done();
+            });
         });
-
-        assert.notEqual(easyideal, null);
-
-        easyideal.execute();
-        easyideal.banks();
     });
 });
