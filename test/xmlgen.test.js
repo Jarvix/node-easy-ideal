d5de01dd994715a2f66f49bd290a9ae218771539
Jos Kuijpers <jos@kuijpersvof.nl>
2014-08-23 14:17:02 +0200

    Use mocha for testing. Implement xml generator.

--- test/xmlgen.test.js
+++ test/xmlgen.test.js
@@ -1,142 +1,117 @@
+/* global describe, it */
 'use strict';
 
 var xmlgen = require('../lib/xmlgen'),
     assert = require('assert');
 
-/**
- * Test helper function.
- *
- * You can change the values after construction.
- *
- * @constructor
- * @param {Object} input - Input to generate xml from.
- * @param {String} output - The expected output.
- * @param {Object} options - Options to pass to the generator.
- */
-function TestHelper(input, output, options) {
-    this.input = input;
-    this.output = output;
-    this.options = options;
+describe('XML generator', function () {
+    describe('Leaf elements', function () {
+        it('should output a single element with contents', function () {
+            assert.equal('<foo>bar</foo>', xmlgen({
+                foo: 'bar'
+            }));
+        });
 
-    this.run = function () {
-        assert.equal(xmlgen(this.input, this.options), this.output);
-    }
-}
+        it('should fold empty elements if fold is true', function () {
+            assert.equal('<foo/>', xmlgen({
+                foo: ''
+            }));
+        });
 
-/**
- * A simple test with a single element and a value.
- */
-(function testSingleElement() {
-    var input = {
-        foo: 'bar'
-    };
-    var output = '<foo>bar</foo>';
-
-    new TestHelper(input, output).run();
-})();
-
-/**
- * Generating an element with no value.
- *
- * This test has two parts: one where folding is expected,
- * and one where folding is turned off using an option.
- */
-(function testEmptyElement() {
-    var input = {
-        foo: ''
-    };
-    var output = '<foo/>';
-
-    var helper = new TestHelper(input, output);
-    helper.run();
-
-    helper.output = '<foo></foo>';
-    helper.options = {
-        fold: false
-    };
-    helper.run();
-})();
-
-/**
- * Test empty input.
- */
-(function testEmpty() {
-    new TestHelper('', '').run();
-})();
-
-/**
- * Test array input.
- */
-(function testArray() {
-    var input = {
-        'book': [
-            'Loner, part one', {
-                title: 'Harry Potter',
-                author: 'J.K. Rowling'
-            }
-        ]
-    };
-
-    var output = '<book>Loner, part one</book><book><title>Harry ';
-    output += 'Potter</title><author>J.K. Rowling</author></book>';
-
-    new TestHelper(input, output).run();
-})();
-
-/**
- * Test an invalid array
- */
-(function testArray() {
-    var input = [{
-        foo: 'bar'
-    }];
-
-    // TODO: assert fail
-})();
-
-/**
- * Test object input.
- */
-(function testObject() {
-
-})();
-
-/**
- * Test the declaration options.
- */
-(function testDeclaration() {
-    var x = xmlgen({
-        foo: 'bar'
-    }, {
-        declaration: true
+        it('should not fold empty elements if fold is false', function () {
+            assert.equal('<foo></foo>', xmlgen({
+                foo: ''
+            }, {
+                fold: false
+            }));
+        });
     });
 
-    assert.equal(x, '<?xml version="1.0" encoding="UTF-8"?><foo>bar</foo>');
-})();
+    describe('Objects', function () {
+        it('should be printed', function () {
+            assert.equal('<foo><a>b</a><c>d</c></foo>', xmlgen({
+                foo: {
+                    a: 'b',
+                    c: 'd'
+                }
+            }));
+        });
+    });
 
-/**
- * Test a real world-case tree.
- */
-(function testTree() {
-    var input = {
-        'Transaction': {
-            'Action': {
-                'Name': 'IDEAL.GETBANKS',
-                'Version': 1
-            },
-            'Merchant': {
-                'ID': 'A',
-                'Key': 'B',
-                'Checksum': 'C'
-            }
-        }
-    };
+    describe('Arrays', function () {
+        it('should work with string elements', function () {
+            assert.equal('<foo>a</foo><foo>b</foo>', xmlgen({
+                foo: ['a', 'b']
+            }));
+        });
 
-    var output = '<?xml version="1.0" encoding="UTF-8"?><Transaction><Action><Name>IDEAL.GETBANKS</Name>';
-    output += '<Version>1</Version></Action><Merchant><ID>A</ID><Key>B</Key><Checksum>C</Checksum>';
-    output += '</Merchant></Transaction>';
+        it('should work with object elements', function () {
+            var out = '<book>Loner, part one</book><book><title>Harry Potter</title>';
+            out += '<author>J.K. Rowling</author></book>';
+            assert.equal(out, xmlgen({
+                'book': [
+                    'Loner, part one', {
+                        title: 'Harry Potter',
+                        author: 'J.K. Rowling'
+                    }
+                ]
+            }));
+        });
+    });
 
-    new TestHelper(input, output, {
-        declaration: true
-    }).run();
-})();
+    describe('Declaration', function () {
+        it('should be hidden if not asked for', function () {
+            assert.equal('<foo>bar</foo>', xmlgen({
+                foo: 'bar'
+            }));
+        });
+        it('should be printed if asked for', function () {
+            assert.equal('<?xml version="1.0" encoding="UTF-8"?><foo>bar</foo>', xmlgen({
+                foo: 'bar'
+            }, {
+                declaration: true
+            }));
+        });
+        it('should use declaration options', function () {});
+    });
+
+    it('should throw an error if root is not an object', function () {
+        assert.throws(function () {
+            xmlgen(5)
+        }, Error);
+        assert.throws(function () {
+            xmlgen('')
+        }, Error);
+        assert.throws(function () {
+            xmlgen([])
+        }, Error);
+    });
+
+    describe('Tree', function () {
+        it('should gracefully print this example', function () {
+            var input = {
+                'Transaction': {
+                    'Action': {
+                        'Name': 'IDEAL.GETBANKS',
+                        'Version': 1
+                    },
+                    'Merchant': {
+                        'ID': 'A',
+                        'Key': 'B',
+                        'Checksum': 'C'
+                    }
+                }
+            };
+
+            var output =
+                '<?xml version="1.0" encoding="UTF-8"?><Transaction><Action><Name>IDEAL.GETBANKS</Name>';
+            output +=
+                '<Version>1</Version></Action><Merchant><ID>A</ID><Key>B</Key><Checksum>C</Checksum>';
+            output += '</Merchant></Transaction>';
+
+            assert.equal(output, xmlgen(input, {
+                declaration: true
+            }));
+        });
+    });
+});
