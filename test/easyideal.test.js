fac788e7dee0870c25c4c5405a151bfa66a26338
Jos Kuijpers <jos@kuijpersvof.nl>
2014-08-25 15:44:42 +0200

    Add transaction support. Skip the tests, as they need a valid id/key/secret set.

--- test/easyideal.test.js
+++ test/easyideal.test.js
@@ -5,22 +5,47 @@ var EasyIdeal = require('../lib/easyideal'),
     assert = require('assert');
 
 describe('Easy Ideal', function () {
-    describe('#banks()', function () {
+    describe.skip('#banks()', function () {
         it('should do stuff', function (done) {
             this.timeout(5000);
             var easyideal = new EasyIdeal({
                 merchant: {
-                    id: 'A',
-                    key: 'A',
-                    secret: 'A'
+                    id: '',
+                    key: '',
+                    secret: ''
                 }
             });
 
             easyideal.banks(function (err, results) {
                 if (err)
                     throw err;
+                done();
+            });
+        });
+    });
 
+    describe.skip('#execute()', function () {
+        it('should do stuff', function (done) {
+            this.timeout(5000);
+            var easyideal = new EasyIdeal({
+                merchant: {
+                    id: '',
+                    key: '',
+                    secret: ''
+                }
+            });
 
+            var transaction = {
+                amount: 8,
+                currency: 'EUR',
+                description: 'Mocha Test',
+                return :'http://www.google.com',
+                bank: 'ING'
+            };
+
+            easyideal.execute(transaction, function (err, bankurl) {
+                if (err)
+                    throw err;
 
                 done();
             });
